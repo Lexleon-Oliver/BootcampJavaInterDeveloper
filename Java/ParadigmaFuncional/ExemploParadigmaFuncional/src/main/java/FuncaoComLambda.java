cfd46e88e9057836f536a2a2c65ef8efb0a21746
Lexleon Óliver <lexleonmg@gmail.com>
2021-02-27 14:40:21 -0300

    Lambda no Java

---
+++ Java/ParadigmaFuncional/ExemploParadigmaFuncional/src/main/java/FuncaoComLambda.java
@@ -0,0 +1,6 @@
+public class FuncaoComLambda {
+    public static void main(String[] args) {
+        Funcao colocarPrefixoSenhorNaString = valor -> "Sr "+valor;
+        System.out.println(colocarPrefixoSenhorNaString.gerar("Joao"));
+    }
+}
